fc805dac40d204d0daef4dccfa151f55338ca98c
许晓楠 <xuxiaonanvip@163.com>
2023-06-30 20:43:02 +0800

    数据库配置完成

--- kehuxufei/models.py
+++ kehuxufei/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class khxx(models.Model):
+    kh_name = models.CharField(max_length=200)
+    kh_url = models.CharField(max_length=200)
+    kh_username = models.CharField(max_length=100)
+    kh_password = models.CharField(max_length=100)
+    kh_datetime = models.DateTimeField(default=timezone.now)
--- kehuxufei/views.py
+++ kehuxufei/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from kehuxufei.models import khxx
 
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse("你好")
+    e = kh_db_xinzeng('品优1','http://8.141.82.239:8899/',"1",'gyljcgtdcpb@acewill.cn')
+    return HttpResponse(e)
+
+# 新增客户方法
+def kh_db_xinzeng(name,url,username,password):
+
+    # 判断客户是否存在，如果存在不可以添加
+    if len(khxx.objects.filter(kh_name=name)) <= 0:
+        db = khxx()
+        db.kh_name = name  # '品优'
+        db.kh_url = url  # 'http://8.141.82.239:8899/'
+        db.kh_username = username  # '1'
+        db.kh_password = password  # 'gyljcgtdcpb@acewill.cn'
+        db.save()
+        return "数据添加成功"
+    else:
+        return "客户已经存在，添加失败"
+
+#修改客户方法
+def kh_db_xiugai(name):
+    db = khxx.objects.filter(kh_name=name)
